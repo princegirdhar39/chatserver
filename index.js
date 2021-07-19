0e82e8fd099b7fcde9d88732e880f105454e26ff
prince <prince@princes-iMac.local>
2021-07-20 04:12:26 +0530

    complete

--- index.js
+++ index.js
@@ -1,15 +1,64 @@
 const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users.js");
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 const router = require("./router");
 
 const server = http.createServer(app);
+corsOptions = {
+  cors: true,
+  origins: ["http://localhost:3000"],
+};
+const io = socketio(server, corsOptions);
 
-const io = socketio(server);
+io.on("connection", (socket) => {
+
+  socket.on("join", ({ name, room }, callback) => {
+
+    const {user,error} = addUser({ id: socket.id, name, room });
+
+    if (error) return callback(error);
+    
+    //default admin messages
+    // we here are emiiting events from the backwend to the frontend
+
+    socket.emit('message', {user: 'admin',text: `${user.name}, welcome to the room ${user.room}`})
+    
+    socket.broadcast.to(user.room).emit('message', {user: 'admin', text : `${user.name} has joined!`});
+    socket.join(user.room);
+    io.to(user.room).emit('roomData', {room:user.room, users: getUsersInRoom(user.room)})
+   
+
+    callback();
+  });
+  //user genarated messages
+  //we are expecting the event in the backend
+  socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit('message', { user: user.name, text: message });
+    io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+
+
+    callback();
+  });
+
+  socket.on("disconnect", () => {
+      const user = removeUser(socket.id);
+      if(user){
+          io.to(user.room).emit('message', {user: 'admin',text: `${user.name} has left `})
+      }
+    console.log("user has left");
+  });
+});
+
 app.use(router);
 
 server.listen(PORT, () => {
   console.log(`server has started on port ${PORT}`);
 });
+
+
